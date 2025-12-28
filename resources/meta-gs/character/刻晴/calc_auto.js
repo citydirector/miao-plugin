6234f5ea36247cfe032b9175d4f0c516b0e0c8f8
KN9527 <35449251+knzakq@users.noreply.github.com>
2025-12-28 18:01:05 +0800

    Update calc_auto.js

--- resources/meta-gs/character/刻晴/calc_auto.js
+++ resources/meta-gs/character/刻晴/calc_auto.js
@@ -104,4 +104,81 @@ export const buffs = [{
     ePlus: 3000,
     qPlus: 3000
   }
+}, {
+  title: '刻皇希杜Q总伤害',
+  params: { q: 1, team: true },
+  dmg: ({ talent }, dmg) => {
+    let t1j = dmg(talent.q['技能伤害'], 'q')
+    let t2j = dmg(talent.q['连斩伤害'] / 8, 'q')
+    let t2 = dmg(talent.q['连斩伤害'] / 8, 'q')
+    let t3j = dmg(talent.q['最后一击伤害'], 'q')
+    return {
+      dmg: t1j.dmg + t2j.dmg * 2 + t2.dmg * 6 + t3j.dmg,
+      avg: t1j.avg + t2j.avg * 2 + t2.avg * 6 + t3j.avg
+    }
+  }
+}]
+
+export const defParams = {
+  q: 1,
+  team: true
+}
+
+export const defDmgIdx = 4
+export const mainAttr = 'atk,cpct,cdmg'
+
+export const buffs = [{
+  title: '刻晴被动：释放Q获得15%暴击率',
+  data: {
+    qCpct: 15
+  }
+}, {
+  title: '刻晴4命：触发雷元素相关反应提升攻击力25%',
+  cons: 4,
+  data: {
+    atkPct: 25
+  }
+}, {
+  title: '刻晴6命：4层获得24%雷伤加成',
+  cons: 6,
+  data: {
+    dmg: 24
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  title: '精5终末6命皇女：增加[atkPct]%攻击',
+  data: {
+    atkPct: 85
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  title: '皇女-千岩牢固：增加攻击[atkPct]%',
+  data: {
+    atkPct: 20
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  title: '精5岩峰巡歌6命希诺宁：[kx]%减抗，[dmg]%增伤',
+  data: {
+    kx: 45,  // 减抗
+    dmg: 51.2   // 增伤
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  title: '希诺宁-烬城勇者绘卷：[dmg]%增伤',
+  data: {
+    dmg: 40
+  }
+}, {
+  check: ({ params }) => params.team === true,
+  title: '精5黑蚀6命杜林：减抗[kx]%，增伤[dmg]%，减防[enemyDef]%，增加攻击[atkPct]%，提升固定伤害',
+  data: {
+    kx: 35,
+    dmg: 50,
+    enemyDef: 30,
+    atkPct: 56,
+    aPlus: 3500,
+    ePlus: 3500,
+    qPlus: 3500
+  }
 }, 'aggravate']
