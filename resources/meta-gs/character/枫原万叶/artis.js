860dbc775128f42f9976325e4fcc513a87e3a6cb
KN9527 <35449251+knzakq@users.noreply.github.com>
2025-12-31 16:47:10 +0800

    Update artis.js

--- resources/meta-gs/character/枫原万叶/artis.js
+++ resources/meta-gs/character/枫原万叶/artis.js
@@ -2,7 +2,7 @@ import { usefulAttr } from "../../artifact/artis-mark.js"
 
 export default function ({ cons, rule, def }) {
   if (cons === 6) {
-    return rule('万叶-满命', { atk: 75, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, recharge: 55 })
+    return rule('万叶-满命', { atk: 85, cpct: 100, cdmg: 100, mastery: 100, dmg: 100, recharge: 75 })
   }
   return def(usefulAttr['枫原万叶'])
 }
