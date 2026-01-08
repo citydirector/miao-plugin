44f3294ac49f7be7dbb45df2635eb401f1e641c5
citydirector <45933075+citydirector@users.noreply.github.com>
2026-01-08 09:03:27 -0800

    Refactor calc.js for clarity and structure

--- resources/meta-gs/character/刻晴/calc.js
+++ resources/meta-gs/character/刻晴/calc.js
@@ -1,36 +1,48 @@
-export const details = [{
-  title: 'E后重击伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2')
-}, {
-  title: 'Q单段伤害',
-  dmg: ({ talent }, dmg) => dmg(talent.q['连斩伤害2'][0], 'q')
-}, {
-  title: 'Q总伤害',
-  params: { q: 1 },
-  dmg: ({ talent }, dmg) => dmg(talent.q['技能伤害'] + talent.q['连斩伤害'] + talent.q['最后一击伤害'], 'q')
-}, {
- 
-}
-}]
+export const details = [
+  {
+    title: 'E后重击伤害',
+    dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2')
+  },
+  {
+    title: 'Q单段伤害',
+    dmg: ({ talent }, dmg) => dmg(talent.q['连斩伤害2'][0], 'q')
+  },
+  {
+    title: 'Q总伤害',
+    params: { q: 1 },
+    dmg: ({ talent }, dmg) =>
+      dmg(
+        talent.q['技能伤害'] +
+          talent.q['连斩伤害'] +
+          talent.q['最后一击伤害'],
+        'q'
+      )
+  }
+]
 
-export const defDmgIdx = 3
+export const defDmgIdx = 2
 export const mainAttr = 'atk,cpct,cdmg,mastery'
 
-export const buffs = [{
-  title: '刻晴被动：释放Q获得15%暴击率',
-  data: {
-    qCpct: 15
-  }
-}, {
-  title: '刻晴4命：触发雷元素相关反应提升攻击力25%',
-  cons: 4,
-  data: {
-    atkPct: 25
-  }
-}, {
-  title: '刻晴6命：4层获得24%雷伤加成',
-  cons: 6,
-  data: {
-    dmg: 24
-  }
-}, 'aggravate']
+export const buffs = [
+  {
+    title: '刻晴被动：释放Q获得15%暴击率',
+    data: {
+      qCpct: 15
+    }
+  },
+  {
+    title: '刻晴4命：触发雷元素相关反应提升攻击力25%',
+    cons: 4,
+    data: {
+      atkPct: 25
+    }
+  },
+  {
+    title: '刻晴6命：4层获得24%雷伤加成',
+    cons: 6,
+    data: {
+      dmg: 24
+    }
+  },
+  'aggravate'
+]
